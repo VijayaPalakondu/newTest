d2a4654bb6af9df427a3bde0546c5f02cb08b542
VijayaPalakondu <vijaya@browserstack.com>
2022-05-18 12:45:47 +0530

    third message

--- ThirdTest.js
+++ ThirdTest.js
@@ -1,3 +1,4 @@
+const { until } = require('selenium-webdriver');
 const {Builder, By, Key} = require('selenium-webdriver');
 require('geckodriver');
 //Click on checkbox and radiobutton
@@ -7,13 +8,25 @@ require('geckodriver');
 (async function test(){
     let driver = await new Builder().forBrowser('firefox').build();
 
-    driver.get("https://www.flipkart.com/");
+    await driver.get("https://www.flipkart.com/");
 
-    driver.findElement(By.xpath("//*[contains(@class, '_2KpZ6l _2doB4z')]")).click();
+    await driver.findElement(By.xpath("//*[contains(@class, '_2KpZ6l _2doB4z')]")).click();
 
-    driver.findElement(By.xpath("//*[contains(@src, '22fddf3c7da4c4f4')]")).click();
+    await driver.findElement(By.xpath("//*[contains(@src, '22fddf3c7da4c4f4')]")).click();
 
-    driver.findElement(By.xpath("//*[@class='_3879cV' or contains(text(), 'realme')]")).click();
+    //the belwo method is not working 
+    //await driver.findElement(By.xpath("//*[@class='_3YgSsQ']/div/a/div/img[contains(@src,'0f46b1ccbe7d0620')]")).click();
+    driver.sleep(3000);
+    driver.findElement(By.xpath("//input[@name='q']")).sendKeys("POCO", Key.ENTER);
 
-    //driver.findElement(By.xpath("//*[contains(@title, 'POCO')]")).click();
+    // await driver.manage().setTimeouts( { implicit: 5000 } ,()=>{
+    //     driver.findElement(By.xpath("//div[contains(text(), 'POCO C31 (Royal Blue, 32'  )]")).click();
+    // });
+    //await driver.manage().setTimeouts( { implicit: 10000 } );
+    await driver.wait(until.elementLocated(By.xpath("//div[contains(text(), 'POCO C31 (Royal Blue, 32'  )]")),30000);
+    await driver.findElement(By.xpath("//div[contains(text(), 'POCO C31 (Royal Blue, 32'  )]")).click();
+
+    await driver.wait(until.elementLocated(By.xpath("//input[@id='pincodeInputId']")),30000);
+    await driver.findElement(By.xpath("//input[@id='pincodeInputId']")).sendKeys('516227' , Key.ENTER);
+    
 })();
